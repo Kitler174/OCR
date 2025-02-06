8b983ef5b7ec804341c7b4ee0dc1fc307e0a9dcc
Kitler <120602207+Kitler174@users.noreply.github.com>
2025-02-06 09:37:45 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# keras.datasets.mnist.load_data(path="mnist.npz")
-# https://keras.io/api/datasets/mnist/
-# https://learn.microsoft.com/pl-pl/azure/open-datasets/dataset-mnist?tabs=azureml-opendatasets
-
-import keras
-from  tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
